e44e3c0168700ba77d5f9e720a2cfa910cd558f5
Thomas Moulard <tmoulard@amazon.com>
2019-10-23 15:20:23 -0700

    Remove async/await from the entire codebase
    All actions need to happen in a particular order.
    
    Signed-off-by: Thomas Moulard <tmoulard@amazon.com>

--- src/setup-ros2.ts
+++ src/setup-ros2.ts
@@ -1,7 +1,7 @@
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 
-async function runAptGetInstall(packages: string[]): Promise<number> {
+function runAptGetInstall(packages: string[]): Promise<number> {
    return exec.exec(
         "sudo",
         ["DEBIAN_FRONTEND=noninteractive", "RTI_NC_LICENSE_ACCEPTED=yes",
@@ -10,58 +10,58 @@ async function runAptGetInstall(packages: string[]): Promise<number> {
     )
 }
 
-async function run() {
+function run() {
   try {
-    await exec.exec("sudo", ["apt-get", "update"]);
+    exec.exec("sudo", ["apt-get", "update"]);
 
     // Select a locale supporting Unicode.
-    await exec.exec("sudo", ["locale-gen", "en_US", "en_US.UTF-8"]);
+    exec.exec("sudo", ["locale-gen", "en_US", "en_US.UTF-8"]);
     core.exportVariable('LANG', 'en_US.UTF-8');
 
     // Enforce UTC time for consistency.
-    await exec.exec("sudo", ["bash", "-c", "echo 'Etc/UTC' > /etc/timezone"]);
-    await exec.exec(
+    exec.exec("sudo", ["bash", "-c", "echo 'Etc/UTC' > /etc/timezone"]);
+    exec.exec(
         "sudo",
         ["ln", "-sf", "/usr/share/zoneinfo/Etc/UTC", "/etc/localtime"])
-    await runAptGetInstall(["tzdata"]);
+    runAptGetInstall(["tzdata"]);
 
     // OSRF APT repository is necessary, even when building
     // from source to install colcon, vcs, etc.
-    await runAptGetInstall(["curl", "gnupg2", "lsb-release"]);
-    await exec.exec(
+    runAptGetInstall(["curl", "gnupg2", "lsb-release"]);
+    exec.exec(
         "sudo",
         ["apt-key", "adv", "--keyserver", "hkp://keyserver.ubuntu.com:80",
          "--recv-keys", "C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654"]);
-    await exec.exec(
+    exec.exec(
     "sudo",
     ["bash", "-c",
      `echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros2-latest.list`])
-    await exec.exec("sudo", ["apt-get", "update"]);
+    exec.exec("sudo", ["apt-get", "update"]);
 
     // Install colcon, rosdep, and vcs.
     // colcon and vcs dependencies (e.g. git), as well as
     // base building packages are not pulled by rosdep, so
     // they are also installed during this stage.
-    await runAptGetInstall(
+    runAptGetInstall(
     ["build-essential", "cmake", "git",
      "python3-colcon-common-extensions", "python3-lark-parser", "python3-pip",
      "python3-rosdep", "python3-vcstool", "wget"])
 
      // FastRTPS dependencies
-    await runAptGetInstall(["libasio-dev", "libtinyxml2-dev"])
+    runAptGetInstall(["libasio-dev", "libtinyxml2-dev"])
 
     // Install OpenSplice
-    await runAptGetInstall(["libopensplice69"])
+    runAptGetInstall(["libopensplice69"])
 
     // Install RTI Connext in a non-interactive way.
     // Skipping the license agreement page requires RTI_NC_LICENSE_ACCEPTED to be
     // set to "yes".
     // This package would normally be installed automatically by rosdep, but
     // there is no way to pass RTI_NC_LICENSE_ACCEPTED through rosdep.
-    await runAptGetInstall(["rti-connext-dds-5.3.1"]);
+    runAptGetInstall(["rti-connext-dds-5.3.1"]);
 
     // Install all Python 3 dependencies.
-    await exec.exec(
+    exec.exec(
     "sudo",
     ["pip3", "install", "--upgrade", "argcomplete", "colcon-common-extensions",
      "colcon-mixin", "flake8", "flake8-blind-except", "flake8-builtins",
@@ -71,7 +71,7 @@ async function run() {
      "setuptools", "wheel"]);
 
     // Initializes rosdep
-    await exec.exec("sudo", ["rosdep", "init"])
+    exec.exec("sudo", ["rosdep", "init"])
   } catch (error) {
     core.setFailed(error.message);
   }
