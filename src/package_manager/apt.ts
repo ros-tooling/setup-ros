1dc3824b64db2627a15ad3ee0850bfe4ea9386e1
Thomas Moulard <tmoulard@amazon.com>
2019-11-27 18:01:00 -0800

    Split code into multiple files
    Signed-off-by: Thomas Moulard <tmoulard@amazon.com>

---
+++ src/package_manager/apt.ts
@@ -0,0 +1,58 @@
+import * as utils from "../utils";
+
+const aptCommandLine: string[] = [
+	"DEBIAN_FRONTEND=noninteractive",
+	"RTI_NC_LICENSE_ACCEPTED=yes",
+	"apt-get",
+	"install",
+	"--no-install-recommends",
+	"--quiet",
+	"--yes"
+];
+
+const aptDependencies: string[] = [
+	"build-essential",
+	"clang",
+	"cmake",
+	"git",
+	"lcov",
+	"python3-colcon-common-extensions",
+	"python3-lark-parser",
+	"python3-pip",
+	"python3-rosdep",
+	"python3-vcstool",
+	"wget",
+	// FastRTPS dependencies
+	"libasio-dev",
+	"libtinyxml2-dev",
+	// OpenSplice
+	"libopensplice69",
+	// RTI Connext - required to ensure the installation in non-blocking
+	"rti-connext-dds-5.3.1"
+];
+
+/**
+ * Run apt-get install on list of specified packages.
+ *
+ * This invokation guarantees that APT install will be non-blocking.
+ *
+ * In particular, it automatically accepts the RTI Connext license, which would block forever otherwise.
+ * Skipping the license agreement page requires RTI_NC_LICENSE_ACCEPTED to be set to "yes".
+ * This package would normally be installed automatically by rosdep, but
+ * there is no way to pass RTI_NC_LICENSE_ACCEPTED through rosdep.
+ *
+ * @param   packages        list of Debian pacakges to be installed
+ * @returns Promise<number> exit code
+ */
+export async function runAptGetInstall(packages: string[]): Promise<number> {
+	return utils.exec("sudo", aptCommandLine.concat(packages));
+}
+
+/**
+ * Run ROS 2 APT dependencies.
+ *
+ * @returns Promise<number> exit code
+ */
+export async function installAptDependencies(): Promise<number> {
+	return runAptGetInstall(aptDependencies);
+}
