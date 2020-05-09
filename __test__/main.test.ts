2b12d5799a236acfb22c87298da797faa7eb0bc4
Dan Rose <dan@digilabs.io>
2020-05-08 21:59:05 -0500

    Reformat with prettier
    Signed-off-by: Dan Rose <dan@digilabs.io>

--- __test__/main.test.ts
+++ __test__/main.test.ts
@@ -5,54 +5,47 @@ import * as linux from "../src/setup-ros-linux";
 import * as osx from "../src/setup-ros-osx";
 import * as windows from "../src/setup-ros-windows";
 
+describe("basic workflow tests", () => {
+	beforeAll(() => {
+		jest.spyOn(actions_exec, "exec").mockImplementation(jest.fn());
+	});
 
-describe('basic workflow tests', () => {
-    beforeAll(() => {
-        jest
-            .spyOn(actions_exec, 'exec')
-            .mockImplementation(jest.fn());
-    })
+	afterAll(() => {
+		jest.restoreAllMocks();
+	});
 
-    afterAll(() => {
-        jest.restoreAllMocks();
-    })
+	it("run Linux workflow", async () => {
+		await expect(linux.runLinux()).resolves.not.toThrow();
+	});
 
-    it('run Linux workflow', async () => {
-        await expect(linux.runLinux()).resolves.not.toThrow();
-    })
+	it("run Windows workflow", async () => {
+		await expect(windows.runWindows()).resolves.not.toThrow();
+	});
 
-    it('run Windows workflow', async () => {
-        await expect(windows.runWindows()).resolves.not.toThrow();
-    })
+	it("run macOS workflow", async () => {
+		await expect(osx.runOsX()).resolves.not.toThrow();
+	});
+});
 
-    it('run macOS workflow', async () => {
-        await expect(osx.runOsX()).resolves.not.toThrow();
-    })
-})
+describe("required-ros-distributions/melodic workflow tests", () => {
+	beforeAll(() => {
+		jest.spyOn(actions_exec, "exec").mockImplementation(jest.fn());
+		jest.spyOn(core, "getInput").mockReturnValue("melodic");
+	});
 
-describe('required-ros-distributions/melodic workflow tests', () => {
-    beforeAll(() => {
-        jest
-            .spyOn(actions_exec, 'exec')
-            .mockImplementation(jest.fn());
-        jest
-            .spyOn(core, 'getInput')
-            .mockReturnValue('melodic');
-    })
+	afterAll(() => {
+		jest.restoreAllMocks();
+	});
 
-    afterAll(() => {
-        jest.restoreAllMocks();
-    })
+	it("run Linux workflow", async () => {
+		await expect(linux.runLinux()).resolves.not.toThrow();
+	});
 
-    it('run Linux workflow', async () => {
-        await expect(linux.runLinux()).resolves.not.toThrow();
-    })
+	it("run Windows workflow", async () => {
+		await expect(windows.runWindows()).resolves.not.toThrow();
+	});
 
-    it('run Windows workflow', async () => {
-        await expect(windows.runWindows()).resolves.not.toThrow();
-    })
-
-    it('run macOS workflow', async () => {
-        await expect(osx.runOsX()).resolves.not.toThrow();
-    })
-})
+	it("run macOS workflow", async () => {
+		await expect(osx.runOsX()).resolves.not.toThrow();
+	});
+});
