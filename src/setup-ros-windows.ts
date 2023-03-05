aada80124ae18700baed7aba61d45d48a74fa912
Stefan Hoffmann <stefan.hoffmann@schiller.de>
2023-03-05 20:46:55 +0100

    Update to latest binary ros2 windows distributions
    Signed-off-by: Stefan Hoffmann <stefan.hoffmann@schiller.de>

--- src/setup-ros-windows.ts
+++ src/setup-ros-windows.ts
@@ -7,9 +7,9 @@ import * as pip from "./package_manager/pip";
 import * as utils from "./utils";
 
 const binaryReleases: { [index: string]: string } = {
-	foxy: "https://github.com/ros2/ros2/releases/download/release-foxy-20210902/ros2-foxy-20210902-windows-release-amd64.zip",
+	foxy: "https://github.com/ros2/ros2/releases/download/release-foxy-20221021/ros2-foxy-20221021-windows-release-amd64.zip",
 	humble:
-		"https://github.com/ros2/ros2/releases/download/release-humble-20220523/ros2-humble-20220523-windows-release-amd64.zip",
+		"https://github.com/ros2/ros2/releases/download/release-humble-20230213/ros2-humble-20230127-windows-release-amd64.zip",
 };
 
 const pip3Packages: string[] = ["lxml", "netifaces"];
