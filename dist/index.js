4f40316b24f393000e81445ad63f14cc630ebdc7
Christophe Bedard <christophe.bedard@apex.ai>
2024-03-25 13:49:32 -0700

    Remove some apt packages for Noble
    We can rely on rosdep for:
    
    1. Some flake8 plugins, see:
       https://github.com/ros2/ros2_documentation/pull/3945
    2. libtinyxml2-dev from tinyxml2_vendor
    3. libasio-dev for Fast DDS
    
    Signed-off-by: Christophe Bedard <christophe.bedard@apex.ai>

--- dist/index.js
+++ dist/index.js
@@ -6653,16 +6653,11 @@ const distributionSpecificAptDependencies = {
     noble: [
         // Basic development packages (from ROS 2 source/development setup instructions)
         // ros-dev-tools includes many packages that we needed to include manually in Focal & older
-        "python3-flake8-docstrings",
         "python3-pip",
         "python3-pytest-cov",
         "python3-flake8-blind-except",
-        "python3-flake8-builtins",
         "python3-flake8-class-newline",
-        "python3-flake8-comprehensions",
         "python3-flake8-deprecated",
-        "python3-flake8-import-order",
-        "python3-flake8-quotes",
         "python3-pytest-repeat",
         "python3-pytest-rerunfailures",
         "ros-dev-tools",
@@ -6671,9 +6666,6 @@ const distributionSpecificAptDependencies = {
         "python3-colcon-lcov-result",
         "python3-colcon-meson",
         "python3-colcon-mixin",
-        // FastRTPS dependencies
-        "libasio-dev",
-        "libtinyxml2-dev",
     ],
 };
 const aptRtiConnextDds = {
