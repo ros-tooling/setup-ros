0dc0e152aa29026d067235fbbb153ed182f8aa60
Christophe Bedard <christophe.bedard@apex.ai>
2024-05-23 11:55:25 -0400

    Add support for Jazzy binaries on Windows
    Signed-off-by: Christophe Bedard <christophe.bedard@apex.ai>

--- src/setup-ros-windows.ts
+++ src/setup-ros-windows.ts
@@ -10,6 +10,8 @@ const binaryReleases: { [index: string]: string } = {
 	humble:
 		"https://github.com/ros2/ros2/releases/download/release-humble-20230614/ros2-humble-20230614-windows-release-amd64.zip",
 	iron: "https://github.com/ros2/ros2/releases/download/release-iron-20230523/ros2-iron-20230523-windows-release-amd64.zip",
+	jazzy:
+		"https://github.com/ros2/ros2/releases/download/release-jazzy-20240523/ros2-jazzy-20250523-windows-release-amd64.zip",
 };
 
 const pip3Packages: string[] = ["lxml", "netifaces"];
