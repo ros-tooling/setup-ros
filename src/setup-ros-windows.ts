653f8eccdb8bf0ca0a0ac45b52622d49cafd5c67
Minggang Wang <minggangw@gmail.com>
2025-06-27 16:07:39 +0800

    Bump all Windows binary releases to latest
    - humble
    - iron
    - jazzy
    - kilted
    
    Signed-off-by: Minggang Wang <minggangw@gmail.com>

--- src/setup-ros-windows.ts
+++ src/setup-ros-windows.ts
@@ -8,12 +8,12 @@ import * as utils from "./utils";
 
 const binaryReleases: { [index: string]: string } = {
 	humble:
-		"https://github.com/ros2/ros2/releases/download/release-humble-20230614/ros2-humble-20230614-windows-release-amd64.zip",
-	iron: "https://github.com/ros2/ros2/releases/download/release-iron-20230523/ros2-iron-20230523-windows-release-amd64.zip",
+		"https://github.com/ros2/ros2/releases/download/humble-20250331/ros2-humble-20250331-windows-release-amd64.zip",
+	iron: "https://github.com/ros2/ros2/releases/download/release-iron-20241204/ros2-iron-20241204-windows-release-amd64.zip",
 	jazzy:
-		"https://github.com/ros2/ros2/releases/download/release-jazzy-20240705/ros2-jazzy-20240705-windows-release-amd64.zip",
+		"https://github.com/ros2/ros2/releases/download/release-jazzy-20250430/ros2-jazzy-20250407-windows-release-amd64.zip",
 	kilted:
-		"https://github.com/ros2/ros2/releases/download/release-kilted-beta-20250430/ros2-kilted-beta-20250430-windows-release-amd64.zip",
+		"https://github.com/ros2/ros2/releases/download/release-kilted-20250523/ros2-kilted-20250523-windows-release-amd64.zip",
 };
 
 const pip3Packages: string[] = ["lxml", "netifaces"];
