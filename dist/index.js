1e0e8106a76442fc5161623ca86c23efb7607f9f
Christophe Bedard <bedard.christophe@gmail.com>
2026-05-02 15:58:01 -0700

    Add Ubuntu 26.04 Resolute and switch Rolling to it (#879)
    `rostooling/setup-ros-docker:ubuntu-resolute-latest` doesn't exist yet,
    so don't switch to it yet.
    
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -28610,12 +28610,36 @@ const distributionSpecificAptDependencies = {
         // https://github.com/ros-tooling/setup-ros/issues/685
         "libclang-rt-dev",
     ],
+    resolute: [
+        // Basic development packages (from ROS 2 source/development setup instructions)
+        // ros-dev-tools includes many packages that we needed to include manually in Focal & older
+        "python3-pip",
+        "python3-pytest-cov",
+        "python3-flake8-blind-except",
+        "python3-flake8-class-newline",
+        "python3-flake8-deprecated",
+        "python3-pytest-repeat",
+        "python3-pytest-rerunfailures",
+        "ros-dev-tools",
+        // Additional colcon packages (not included in ros-dev-tools)
+        "python3-colcon-coveragepy-result",
+        "python3-colcon-lcov-result",
+        "python3-colcon-meson",
+        "python3-colcon-mixin",
+        // Others
+        "python3-importlib-metadata",
+    ],
 };
 const aptRtiConnextDds = {
     jammy: ["rti-connext-dds-6.0.1"],
-    // ROS 2 Rolling switched to Connext 7.3.0 before Kilted;
-    // ROS 2 Jazzy still uses Connext 6.0.1
-    noble: ["rti-connext-dds-6.0.1", "rti-connext-dds-7.3.0-ros"],
+    // ROS 2 Jazzy still uses Connext 6.0.1.
+    // ROS 2 Rolling switched to Connext 7.3.0 before Kilted and then switched to Connext 7.7.0 before Lyrical.
+    noble: [
+        "rti-connext-dds-6.0.1",
+        "rti-connext-dds-7.3.0-ros",
+        "rti-connext-dds-7.7.0-ros",
+    ],
+    resolute: ["rti-connext-dds-7.7.0-ros"],
 };
 /**
  * Run apt-get install on list of specified packages.
@@ -29642,7 +29666,7 @@ function runLinux() {
         yield configOs();
         const ubuntuCodename = yield utils.determineDistribCodename();
         yield installRosAptSourcePackageIfNeeded(ubuntuCodename, use_ros2_testing);
-        if ("noble" !== ubuntuCodename) {
+        if ("noble" !== ubuntuCodename && "resolute" !== ubuntuCodename) {
             // Temporary fix to avoid error mount: /var/lib/grub/esp: special device (...) does not exist.
             const arch = yield utils.getArch();
             yield utils.exec("sudo", ["apt-mark", "hold", `grub-efi-${arch}-signed`]);
